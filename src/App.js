85e45985842d8578634bd2b2ed7a2325ad71afe5
Coders.Mickey <thuongnguyen.it78@gmail.com>
2021-01-01 21:49:10 +0700

    done component ColorBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 
 // Ctrl + Shirt + O: sẽ tự động tìm những thằng không sử dụng và bỏ đi
 function App() {
   return (
     <div className="app">
-      Hello World
+      <ColorBox/>
     </div>
     
   );
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,42 @@
+// 1. có một cái khung, sau khi ta click nó sẽ tự thay đổi màu
+// 2. nếu ta reload lại trang, thì trạng thái màu lúc trước vẫn giữ nguyên
+import React, { useState } from 'react';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+    
+};
+
+function randomeColor() {
+    const ARR_COLOR = ['deeppink', 'green', 'yellow', 'black', 'blue']
+    const indexColor = Math.trunc(Math.random() * ARR_COLOR.length)
+    return ARR_COLOR[indexColor]
+}
+
+function ColorBox() {
+
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('color')
+        return initColor
+    })
+
+    function handleColorClick() {
+        const colorRandom = randomeColor()
+        setColor(colorRandom)
+        localStorage.setItem('color', colorRandom)
+        return;
+    }
+
+    return (
+        <div style = {{
+            backgroundColor: color,
+          
+        }}
+        onClick = {handleColorClick}
+        className = "color-box"
+        >
+        </div>
+    );
+}
+
+export default ColorBox;
