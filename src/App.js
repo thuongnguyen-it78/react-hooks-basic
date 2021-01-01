6e933fe0328411e8f477a009e5054786c2e1d068
Coders.Mickey <thuongnguyen.it78@gmail.com>
2021-01-01 20:07:13 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+
+
+// Ctrl + Shirt + O: sẽ tự động tìm những thằng không sử dụng và bỏ đi
+function App() {
+  return (
+    <div className="app">
+      Hello World
+    </div>
+    
+  );
+}
+
+export default App;
